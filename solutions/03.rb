9c12bc3fa7b0735ad1fe4824ec3dfc295414b873
yordan pulov <yordanpulov@gmail.com>
2014-01-22 16:31:36 +0200

    fix lots of white spaces, remove matrix , new bresenham algorithm

--- solutions/03.rb
+++ solutions/03.rb
@@ -1,158 +1,257 @@
-require 'matrix.rb'
-  class Matrix
-    def []= (i,j,x)
-      @rows[i][j] = x
-    end
-  end
 module Graphics
   class Canvas
   attr_reader :height , :width
     def initialize(x,y)
-
-      @layout = Matrix.build(x, y) {false}
-
-      @height  , @width = x , y
+      @layout ={}
+      @height  , @width = y , x
+      self
     end
+
     def set_pixel(x,y)
-      @layout[x,y] = true
+      @layout[[x,y]] = true
       self
     end
+
     def pixel_at?(x,y)
-      @layout[x,y]
-    end
+      @layout[[x,y]]
+     end
+
     def draw(figure)
-      figure.layout.each_with_index do |col,row|
-        draw_inner_loop(col,row) if (col)
+      figure.layout.each do |possition,value|
+
+       set_pixel(possition[0],possition[1]) if value
       end
       self
     end
-    def draw_inner_loop(col,row)
-      col.each { |value| set_pixel(value,row) if value }
-    end
+
     def render_as(renderer)
-      output = renderer::RENDER[:header]
-      @layout.each_with_index do|val , _ , coll|
-        pixel =  val ? renderer::RENDER[:render][1]  :  renderer::RENDER[:render][0]
-        output << pixel
-         output << renderer::RENDER[:newline] if coll == width-1
-      end
-      output<<renderer::RENDER[:footer]
+      renderer.render self
     end
   end
 
   class Point
     attr_reader :x , :y ,:layout
+
     def initialize(x,y)
-      @layout =[]
-      @layout[x]=[y]
+      @layout ={}
+      @layout[[x,y]] = true
       @x , @y = x , y
     end
   end
 
 
+#=== Този алгоритъм е взет от Митъо и е леко преправен така че да пасва на моят canvas
+
+    class BresenhamRasterization
+      def initialize(from_x, from_y, to_x, to_y)
+        @from_x, @from_y = from_x, from_y
+        @to_x, @to_y     = to_x, to_y
+        @layout ={}
+      end
+
+      def rasterize_on(canvas="")
+        initialize_from_and_to_coordinates
+        rotate_coordinates_by_ninety_degrees if steep_slope?
+        swap_from_and_to if @drawing_from_x > @drawing_to_x
+
+        draw_line_pixels_on canvas
+        @layout
+      end
+
+      def steep_slope?
+        (@to_y - @from_y).abs > (@to_x - @from_x).abs
+      end
+
+      def initialize_from_and_to_coordinates
+        @drawing_from_x, @drawing_from_y = @from_x, @from_y
+        @drawing_to_x, @drawing_to_y     = @to_x, @to_y
+      end
+
+      def rotate_coordinates_by_ninety_degrees
+        @drawing_from_x, @drawing_from_y = @drawing_from_y, @drawing_from_x
+        @drawing_to_x, @drawing_to_y     = @drawing_to_y, @drawing_to_x
+      end
+
+      def swap_from_and_to
+        @drawing_from_x, @drawing_to_x = @drawing_to_x, @drawing_from_x
+        @drawing_from_y, @drawing_to_y = @drawing_to_y, @drawing_from_y
+      end
+
+      def error_delta
+        delta_x = @drawing_to_x - @drawing_from_x
+        delta_y = (@drawing_to_y - @drawing_from_y).abs
+
+        delta_y.to_f / delta_x
+      end
+
+      def vertical_drawing_direction
+        @drawing_from_y < @drawing_to_y ? 1 : -1
+      end
+
+      def draw_line_pixels_on(canvas)
+        @error = 0.0
+        @y     = @drawing_from_y
+
+        @drawing_from_x.upto(@drawing_to_x).each do |x|
+          @layout[[x,@y]] = true
+          calculate_next_y_approximation
+        end
+      end
+
+      def calculate_next_y_approximation
+        @error += error_delta
+
+        if @error >= 0.5
+          @error -= 1.0
+          @y += vertical_drawing_direction
+        end
+      end
+
+      def set_pixel_on(canvas, x, y)
+        if steep_slope?
+          @layout[[x,y]] = true
+        else
+          @layout[[y,x]] = true
+        end
+      end
+    end
+
+#===
+
   class Line
     attr_reader :from , :to ,:layout
+
     def initialize(start_point,end_point)
-      @layout =[]
-      @from = start_point
-      (start_point.x).upto(end_point.x) { |i| @layout[i] = [] }
-      if(start_point.x ==end_point.x or start_point.y == end_point.y)
-        then
-        straight_line(start_point,end_point)
+      @layout ={}
+
+      if start_point.x > end_point.x or ( start_point.y > end_point.y)
+        @from = end_point
+        @to   = start_point
       else
-        bresenham(start_point,end_point)
+        @from = start_point
+        @to   = end_point
+      end
+
+
+      if(start_point.x ==end_point.x or start_point.y == end_point.y)
+        straight_line( @from,@to)
+      else
+        @layout =BresenhamRasterization.new( @from.x,  @from.y, @to.x, @to.y).rasterize_on
+
       end
     end
-    def bresenham(start_point,end_point)
-      @horizontal_length=end_point.x-start_point.x
-      @vertical_length=end_point.y-start_point.y
-      @equation = 2*@vertical_length -@horizontal_length
-      @layout[start_point.x] = [start_point.y]
-      @end_point_changing=end_point.y
-      bresenham_line_loop(start_point,end_point)
-     end
-     def bresenham_line_loop(start_point,end_point)
-        (start_point.x+1).upto(end_point.x) do |i|
-        if @equation > 0
-          then
-          bresenham_equation_greater(i)
-        else
-          bresenham_equation_lower(i)
-        end
-        @to = Point.new i , @end_point_changing
-      end
-    end
-    def bresenham_equation_greater(i)
-      @end_point_changing = @end_point_changing+1
-      @layout[i].push(@end_point_changing)
-      @equation = @equation + (2*@vertical_length-2*@horizontal_length)
-    end
-    def bresenham_equation_lower(i)
-       @layout[i].push(@end_point_changing)
-       @equation =@equation + (2*@vertical_length)
-    end
+
+
     def straight_line(start_point,end_point)
-        (start_point.x).upto(end_point.x) { |i| @layout[i] =[start_point.y , end_point.y]    }
-            (start_point.y).upto(end_point.y) do |i|
-        @layout[start_point.x] .push(i)
-        @layout[end_point.x] .push(i)
-      end
-      @to = end_point
+        (start_point.x).upto(end_point.x) { |i|  @layout[[i,start_point.y]] =@layout[[i, end_point.y] ] = true   }
+        (start_point.y).upto(end_point.y) do |i|
+          @layout[[start_point.x, i]] = true
+          @layout[[end_point.x, i]] = true
+        end
+        @to = end_point
+
     end
+
   end
 
 
   class Rectangle
     attr_reader :top_left , :top_right , :bottom_left , :bottom_right ,:layout
-    def initialize(start_point,end_point)
-      @layout =[]
-      @top_left , @top_right = start_point , Point.new(start_point.x,end_point.y)
-      @bottom_left = Point.new(end_point.x,start_point.y)
-      @bottom_right = end_point
-      draw_it(start_point,end_point)
+    def min(x,y)
+      return x if x<y
+      y
     end
+
+    def max(x,y)
+      return y if x<y
+      x
+    end
+
+    def initialize(start_point,end_point)
+        @layout ={}
+
+
+      @bottom_right =Point.new( max(start_point.x , end_point.x), max(start_point.y,end_point.y) )
+      @bottom_left =Point.new( min(start_point.x , end_point.x), max(start_point.y,end_point.y) )
+      @top_left =Point.new( min(start_point.x , end_point.x), min(start_point.y,end_point.y) )
+      @top_right =Point.new( max(start_point.x , end_point.x), min(start_point.y,end_point.y) )
+
+
+      draw_it(@top_left,@bottom_right)
+    end
+
     def draw_it(start_point,end_point)
-      (start_point.x).upto(end_point.x) { |i| @layout[i] =[start_point.y , end_point.y]    }
+      (start_point.x).upto(end_point.x) { |i| @layout[[i,start_point.y]] = @layout[[i, end_point.y]] = true   }
       (start_point.y).upto(end_point.y) do |i|
-        @layout[start_point.x] .push(i)
-        @layout[end_point.x] .push(i)
+        @layout[[start_point.x,i]] = true
+        @layout[[end_point.x,i]] = true
       end
     end
+
   end
 
 
   module Renderers
+
     module Ascii
-      RENDER = {footer: "" , header: "" , render: ["-" ,"@"] , newline: "\n"}
+
+      def self.render(canvas)
+        output = ""
+        0.upto(canvas.height-1) do|i|
+          0.upto(canvas.width-1) do |j|
+            pixel =  canvas.pixel_at?(j,i) ? "@" :  "-"
+            output << pixel
+          end
+           output << "\n" unless i == canvas.height-1
+        end
+        output
+      end
      end
+
     module Html
        RENDER = {footer: " </div>
-  </body>
-  </html>" , header: '  <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Rendered Canvas</title>
-    <style type="text/css">
-      .canvas {
-  font-size: 1px;
-  line-height: 1px;
-      }
-      .canvas * {
-  display: inline-block;
-  width: 10px;
-  height: 10px;
-  border-radius: 5px;
-      }
-      .canvas i {
-  background-color: #eee;
-      }
-      .canvas b {
-  background-color: #333;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="canvas">' , render: ["<i></i>" ,"<b></b>"] , newline: "<br/> \n"}
+                </body>
+                </html>" , header: '  <!DOCTYPE html>
+                <html>
+                <head>
+                  <title>Rendered Canvas</title>
+                  <style type="text/css">
+                    .canvas {
+                font-size: 1px;
+                line-height: 1px;
+                    }
+                    .canvas * {
+                display: inline-block;
+                width: 10px;
+                height: 10px;
+                border-radius: 5px;
+                    }
+                    .canvas i {
+                background-color: #eee;
+                    }
+                    .canvas b {
+                background-color: #333;
+                    }
+                  </style>
+                </head>
+                <body>
+                  <div class="canvas">'
+                }
+
+    def self.render(canvas)
+        output = RENDER[:header]
+        0.upto(canvas.height-1) do|i|
+          0.upto(canvas.width-1) do |j|
+            pixel =  canvas.pixel_at?(j,i) ? "<b></b>" : "<i></i>"
+            output << pixel
+
+          end
+          output << "<br>" unless i == canvas.height-1
+        end
+        output<<RENDER[:footer]
+
+      end
+
     end
   end
 
